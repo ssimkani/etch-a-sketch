50a1d4c020df45c85c69635179f9c8f8f47f55a2
Seeba184 <seenasimkani@gmail.com>
2024-03-24 15:02:13 -0400

    Append grid divs to container div

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const container = document.querySelector("#container");
 
 for (let row = 0; row < 16; row++) {
-    for (let col = 0; col< 16; col++) {
-        const newDiv = document.createElement("div")
-        newDiv.classList.add("grid-boxes")
-        
-    }
+  for (let col = 0; col < 16; col++) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("grid-boxes");
+    container.appendChild(newDiv);
+  }
 }
