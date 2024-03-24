4f400a4950d96944840e9458b73952a8b22a6fd6
Seeba184 <seenasimkani@gmail.com>
2024-03-24 16:10:22 -0400

    Add eventListeners for all buttons

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const container = document.querySelector("#container");
-const btnEraser = document.querySelector("#eraser")
-const btnClear = document.querySelector("#clear")
-const btnDraw = document.querySelector("#draw")
+const btnEraser = document.querySelector("#eraser");
+const btnClear = document.querySelector("#clear");
+const btnDraw = document.querySelector("#draw");
 
 for (let row = 0; row < 16; row++) {
   for (let col = 0; col < 16; col++) {
@@ -11,7 +11,18 @@ for (let row = 0; row < 16; row++) {
     newDiv.addEventListener("mouseover", () => {
       newDiv.style.backgroundColor = "blue";
     });
-    
-
+    btnEraser.addEventListener("click", () => {
+      newDiv.addEventListener("mouseover", () => {
+        newDiv.style.backgroundColor = "white";
+      });
+    });
+    btnDraw.addEventListener("click", () => {
+      newDiv.addEventListener("mouseover", () => {
+        newDiv.style.backgroundColor = "blue";
+      });
+    });
+    btnClear.addEventListener("click", () => {
+        newDiv.style.backgroundColor = "white";
+    })
   }
 }
