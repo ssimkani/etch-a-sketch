d7b6a13e524f56c62c7d0df3496c0b0ee9fb447c
Seeba184 <seenasimkani@gmail.com>
2024-03-24 16:28:35 -0400

    Add only integer values to be entered into input field up to 100

--- index.js
+++ index.js
@@ -3,28 +3,36 @@ const btnEraser = document.querySelector("#eraser");
 const btnClear = document.querySelector("#clear");
 const btnDraw = document.querySelector("#draw");
 const changeGridSize = document.querySelector("#change-grid");
-
+const enterGridSize = document.querySelector("#enter-grid-size");
 
 for (let row = 0; row < 16; row++) {
   for (let col = 0; col < 16; col++) {
     const newDiv = document.createElement("div");
     newDiv.classList.add("grid-boxes");
     container.appendChild(newDiv);
+
     newDiv.addEventListener("mouseover", () => {
       newDiv.style.backgroundColor = "blue";
     });
+
     btnEraser.addEventListener("click", () => {
       newDiv.addEventListener("mouseover", () => {
         newDiv.style.backgroundColor = "white";
       });
     });
+
     btnDraw.addEventListener("click", () => {
       newDiv.addEventListener("mouseover", () => {
         newDiv.style.backgroundColor = "blue";
       });
     });
+
     btnClear.addEventListener("click", () => {
       newDiv.style.backgroundColor = "white";
+    });
+
+    enterGridSize.addEventListener("click", () => {
+      const inputValue = changeGridSize.value;
     });
   }
 }
